c61dfb313793f4e624aaeb94c037ef30186b1d82
HomeFinder <167259553+Zali0@users.noreply.github.com>
2025-08-18 19:00:23 +0000

    new

--- routes/Auth.js
+++ routes/Auth.js
@@ -31,7 +31,7 @@ route.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'Lax',
+      sameSite: 'None',
       secure: false, // Set to true in production
       maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000,
     });
